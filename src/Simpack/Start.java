e137389cf0a169a7b89aaeaac39c2a2858b50027
linalabar <lina.labar@ensia.edu.dz>
2023-03-06 15:13:37 +0100

    comment added to Start

--- src/Simpack/Start.java
+++ src/Simpack/Start.java
@@ -3,7 +3,10 @@ package Simpack;
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Hello World!");
+		// comment1
+		// comment2
+		// comment3
 
 	}
 
